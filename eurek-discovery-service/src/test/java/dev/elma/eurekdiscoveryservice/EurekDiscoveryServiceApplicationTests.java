63cfe50cfd5f72c371bb4ec4c84f9e4ab8695ff8
Elma-dev <67378945+Elma-dev@users.noreply.github.com>
2023-10-16 23:42:00 +0100

    commit last

---
+++ eurek-discovery-service/src/test/java/dev/elma/eurekdiscoveryservice/EurekDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.eurekdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

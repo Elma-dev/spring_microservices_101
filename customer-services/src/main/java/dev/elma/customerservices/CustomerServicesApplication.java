29052029ab54714da2c13d192c57f5a857d4ecb5
Elma-dev <67378945+Elma-dev@users.noreply.github.com>
2023-10-16 16:49:26 +0100

    commit

--- customer-services/src/main/java/dev/elma/customerservices/CustomerServicesApplication.java
+++ customer-services/src/main/java/dev/elma/customerservices/CustomerServicesApplication.java
@@ -1,13 +1,30 @@
 package dev.elma.customerservices;
 
+import dev.elma.customerservices.entities.Customer;
+import dev.elma.customerservices.repositories.CustomerJpaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CustomerServicesApplication {
+import java.util.stream.Stream;
 
+@SpringBootApplication
+@AllArgsConstructor
+public class CustomerServicesApplication implements CommandLineRunner {
+    CustomerJpaRepository customerJpaRepository;
     public static void main(String[] args) {
         SpringApplication.run(CustomerServicesApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        Stream.of("Abdeljalil","Ayoub","Hicham").forEach(
+                name->{
+                    Customer c=Customer.builder().name(name).email(name+"@gmail.com").build();
+                    customerJpaRepository.save(c);
+                }
+        );
+    }
 }

1186651d7bda6a52a3346b5ae47af6216847e3db
Elma-dev <67378945+Elma-dev@users.noreply.github.com>
2023-10-21 12:52:21 +0100

    beggin app 2

---
+++ Application_Architecture_Consul_Config_Gateway_Feign_Resilience4J/configuration_ms/src/main/java/dev/elma/configuration_ms/ConfigurationMsApplication.java
@@ -0,0 +1,13 @@
+package dev.elma.configuration_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationMsApplication.class, args);
+	}
+
+}
---
+++ Application_Architecture_Consul_Config_Gateway_Feign_Resilience4J/configuration_ms/src/test/java/dev/elma/configuration_ms/ConfigurationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.configuration_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
